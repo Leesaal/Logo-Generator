5c0aebb9b71dd340b66e2005512d4ed23274e714
Leesa A <leesaali7@gmail.com>
2023-04-06 19:37:55 +0100

    generate an svg file with data

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const inquirer = require("inquirer");
 const {Triangle, Circle, Square} = require("./lib/shapes");
 const fs = require("fs");
 
-class Logo{
+class Logo {
     constructor() {
-        this.textElement = "";
-        this.shapeElement = "";
+        this.text = "";
+        this.textColour = "";
     }
+
+    setShape(shape, shapeColour) {
+        this.shape = shape.showShape(shapeColour);
+    }
+
     createLogo() {
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="400">
-            ${this.textElement}
-            ${this.shapeElement}
-        </svg>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+  ${setShape(shape, shapeColour)}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>
+  </svg>`
     }
-    setTextElement(text, textColour) {
-        this.textElement = `<text x="150" y="150" font-family="Serif" font-size="40" fill="${this.textColour}">${this.text}</text> text-anchor="middle"`;
-    }
-    setShapeElement(shape, shapeColour) {
-        this.shapeElement = shape.showShape();
-    }
+
 }
 
 const questions = [
@@ -30,7 +30,7 @@ const questions = [
     },
     {
         type: "input",
-        name: "text-colour",
+        name: "textColour",
         message: "Enter a colour for the text: "
     },
     {
@@ -41,17 +41,40 @@ const questions = [
     },
     {
         type: "input",
-        name: "shape-colour",
+        name: "shapeColour",
         message: "Enter a colour for the shape: "
     }
 ];
+
+
+
+function generateLogo(answers, newLogo) {
+    this.text = answers.text.toUpperCase();
+    this.textColour = answers.textColour;
+    fs.writeFile("./examples/logo.svg", `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+  ${new Triangle("pink")}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>
+  </svg>`, (e) => {
+    e ? console.log(e) : console.log("Successfully created logo", "Generated logo.svg")
+  });
+}
 
 function init() {
     inquirer
     .prompt (questions)
     .then ((answers) => {
-        eval("svg = new " + answers.shape + "(answers.shape, answers.shapeColour, answers.text, answers.textColour)");
-        fs.writeFile("logo.svg", answers);
+        if (answers.shape == "Triangle") {
+            const newLogo = new Triangle(answers.shapeColour);
+            generateLogo(answers, newLogo);
+        }
+        if (answers.shape == "Square") {
+            const newLogo = new Square(answers.shapeColour);
+            generateLogo(answers, newLogo);
+        }
+        if (answers.shape == "Circle") {
+            const newLogo = new Circle(answers.shapeColour);
+            generateLogo(answers, newLogo);
+        }
     })
     .catch((err)=> console.log(err));
 }
